7b5afeb74c2b8b0e943fdda633352f4e283ea6eb
weilaystudio.com <ignaciosergiodiaz@gmail.com>
2021-03-31 03:03:34 -0300

    change in database URL

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,10 @@
 import {connect } from 'mongoose'
 
+const UrlProd = "mongodb+srv://admin:<User_Admin1>@cluster0.zojpg.mongodb.net/rokoop_db?retryWrites=true&w=majority" ;
+const UrlDev = "mongodb://localhost:27017/photo-gallery-db";
+
 export async function startConnection(){
-    await connect('mongodb://localhost:27017/photo-gallery-db', {
+    await connect(UrlProd, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false

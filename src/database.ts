8e3e86a737c21b6e7f4251c12320d288e2c98e27
weilaystudio.com <ignaciosergiodiaz@gmail.com>
2021-03-31 03:08:04 -0300

    correction data

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import {connect } from 'mongoose'
 
-const UrlProd = "mongodb+srv://admin:<User_Admin1>@cluster0.zojpg.mongodb.net/rokoop_db?retryWrites=true&w=majority" ;
+const UrlProd = "mongodb+srv://admin:<admin1234>@cluster0.zojpg.mongodb.net/rokoop_db?retryWrites=true&w=majority" ;
 const UrlDev = "mongodb://localhost:27017/photo-gallery-db";
 
 export async function startConnection(){
